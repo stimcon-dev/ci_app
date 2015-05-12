1af499080a0ec0b4ff415248c0a4672ad20b918f
hiren Suhagiya <dev2@stimulusconsultancy.com>
2015-05-12 18:16:29 +0530

    Second Commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,12 +68,6 @@
 
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<h2>Welcome to my first CI-APP</h2>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
 </body>
